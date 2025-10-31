d4fb9f6cd539e28ebfd259e80fb30f4612614dc7
Ievgen German <german.kbrza@gmail.com>
2025-10-31 12:33:28 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-#  write your code here
+class Animal:
+    def __init__(self, name, appetite: int, is_hungry: bool) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+def print_name():
+    pass
+
+def feed():
+    pass
